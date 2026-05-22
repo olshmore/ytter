56a24da736c34d3db4f7992f6f5371a5f7fec018
Olga Shaposhnikova <olshmore@gmail.com>
2026-05-22 10:44:54 +0100

    embed iana db timezones

--- main.go
+++ main.go
@@ -33,6 +33,8 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	_ "time/tzdata"
 )
 
 var interruptSignals = []os.Signal{
